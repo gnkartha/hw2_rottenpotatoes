fa18e43e8ebdbcfb3667ab8b42ba5c1890545a84
Neelakantan Kartha <gnkartha@yahoo.com>
2012-03-12 02:05:12 -0700

    Added hiliting code

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,32 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def title_hilite
+    sorting_key = params[:sort_by]
+    if (sorting_key == "title")
+     if @release_hilite == true
+       @release_hilite = false
+     end
+     @title_hilite = true
+    end
+    if @title_hilite
+      return "hilite"
+    else
+      return ""
+    end
+  end
+  def release_hilite
+    sorting_key = params[:sort_by]
+    if (sorting_key == "release_date")
+     if @title_hilite == true
+       @title_hilite = false
+     end
+     @release_hilite = true
+    end
+    if @release_hilite
+      return "hilite"
+    else
+      return ""
+    end
+  end
 end
