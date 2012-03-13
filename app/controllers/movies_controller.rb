8239c12265fdb234a7fe9cd257daa1da6d02c208
Neelakantan Kartha <gnkartha@yahoo.com>
2012-03-12 21:45:15 -0700

    Added code for Part 4 of HW2: filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,15 +7,23 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @checked_hash = params[:ratings]
+    if @checked_hash != nil
+      checked_array = @checked_hash.keys
+    else
+      checked_array = @all_ratings
+    end
+    #debugger
     sorting_key = params[:sort_by]
-    # puts "NKtest = " + sorting_key.to_s  #Testing git
     # @movies = Movie.all  # this was the old code
     if (sorting_key == "title")
-     @movies = Movie.find(:all, :order => "title")
+     @movies = Movie.find(:all, :order => "title", :conditions => {:rating => checked_array})
+    # debugger
     elsif (sorting_key == "release_date")
-     @movies = Movie.find(:all, :order => "release_date")
-    else
-     @movies = Movie.all
+     @movies = Movie.find(:all, :order => "release_date", :conditions => {:rating => checked_array})
+    elsif
+      @movies = Movie.find(:all, :conditions => {:rating => checked_array})
     end
   end
 
